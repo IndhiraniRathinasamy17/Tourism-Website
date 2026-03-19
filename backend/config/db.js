7361b24329e3f1e240aabb6e7971e115c44afb39
IndhiraniRathinasamy17 <rathinasamyindhirani@gmail.com>
2026-03-19 21:01:04 +0530

    added

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,23 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect("mongodb+srv://Indhirani:Indhi%4017@cluster0.1piqu7r.mongodb.net/Users?appName=Cluster0");
+//     console.log("MongoDB Connected");
+//   } catch (error) {
+//     console.log(error);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
+import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://Indhirani:Indhi%4017@cluster0.1piqu7r.mongodb.net/Users?appName=Cluster0");
+    await mongoose.connect(
+      "mongodb+srv://Indhirani:Indhi%4017@cluster0.1piqu7r.mongodb.net/Users?appName=Cluster0"
+    );
     console.log("MongoDB Connected");
   } catch (error) {
     console.log(error);
@@ -10,4 +25,4 @@ const connectDB = async () => {
   }
 };
 
-module.exports = connectDB;
+export default connectDB;
